d8b07f84f7a649c325b334bddbf794a503a55b5f
GondorGitaros <csaszardaniel81@gmail.com>
2023-12-10 18:30:27 +0100

    made it better

--- alt+u.py
+++ alt+u.py
@@ -15,36 +15,34 @@ def addone():
 def copy(this):
     pyperclip.copy(this)
 
-def typethis(this):
-    copy(this)
-    keyboard.press('ctrl')
-    time.sleep(0.1)
-    keyboard.press('v')
-    time.sleep(0.1)
-    keyboard.release('v')
-    time.sleep(0.1)
-    keyboard.release('ctrl')
-    time.sleep(0.1)
-    keyboard.press('enter')
-    time.sleep(0.1)
-    keyboard.release('enter')
-
 def s():
     time.sleep(0.4)
 
+def press(keybind):
+    keyboard.press(keybind)
+    s()
+
+def release(keybind):
+    keyboard.release(keybind)
+    s()
+
+def typethis(this):
+    copy(this)
+    press('ctrl')
+    press('v')
+    release('v')
+    release('v')
+    press('enter')
+    release('enter')
+
 def open_terminal():
     time.sleep(0.5)
-    keyboard.press('ctrl')
-    s()
-    keyboard.press('j')
-    s()
-    keyboard.release('j')
-    s()
-    keyboard.press('j')
-    s()
-    keyboard.release('j')
-    s()
-    keyboard.release('ctrl')
+    press('ctrl')
+    press('j')
+    release('j')
+    press('j')
+    release('j')
+    release('ctrl')
 
 
 #get counter
@@ -64,18 +62,12 @@ while True:
         command4 = "code app.js "
         command6 = "cp style.css " + counter + "/style.css"
         open_terminal()
-        s()
         typethis("cd ..")
         time.sleep(0.5)
         typethis(command1)
-        s()
         typethis(command2)
-        s()
         typethis(command6)
-        s()
         typethis(command3)
-        s()
         typethis(command4)
-        s()
         addone()
         time.sleep(0.5)
