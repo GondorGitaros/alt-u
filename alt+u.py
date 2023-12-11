32b4c5998a78e8e15f6aa888fbeccdd8d05d8311
GondorGitaros <csaszardaniel81@gmail.com>
2023-12-11 21:49:38 +0100

    Made big changes to code

--- alt+u.py
+++ alt+u.py
@@ -1,70 +1,37 @@
 import keyboard
 import time
-import pyperclip
+import os
+import shutil
+import subprocess
 
-FOLDER_LOCATION = "D:\Code\Git\Main\jslearn"
+FOLDER_LOCATION = "D:\\Code\\Git\\Main\\jslearn\\"
+COUNTER_FILE_LOCATION = "D:\\Code\\Git\\Main\\alt+u\\counter.txt"
+INDEX_HTML_PATH = "D:\\Code\\Git\\Main\\jslearn\\index.html"
+STYLE_CSS_PATH = "D:\\Code\\Git\\Main\\jslearn\\style.css"
 
 #increment counter by 1 and save it to counter.txt
 def addone():
-    with open('counter.txt', 'r') as file:
+    with open(COUNTER_FILE_LOCATION , 'r') as file:
         counter = file.read()
 
     addone = counter.replace(counter, str(int(counter) + 1))
 
-    with open('counter.txt', 'w') as file:
+    with open(COUNTER_FILE_LOCATION, 'w') as file:
         file.write(addone)
 
-def copy(this):
-    pyperclip.copy(this)
-
-def s():
-    time.sleep(0.4)
-
-def pressandrelease(keybind):
-    keyboard.press(keybind)
-    time.sleep(0.3)
-    keyboard.release(keybind)
-    time.sleep(0.3)
-
-def press(keybind):
-    keyboard.press(keybind)
-    s()
-
-def release(keybind):
-    keyboard.release(keybind)
-    s()
-
-def typethis(this):
-    copy(this)
-    press('ctrl')
-    pressandrelease('v')
-    release('ctrl')
-    pressandrelease('enter')
-
-def open_terminal():
-    time.sleep(0.5)
-    press('ctrl')
-    pressandrelease('j')
-    pressandrelease('j')    
-    release('ctrl')
-
 #get counter
-with open('counter.txt', 'r') as file:
+with open(COUNTER_FILE_LOCATION, 'r') as file:
     counter = file.read()
+
+path = os.path.join(FOLDER_LOCATION, counter) + "\\"
+JS_PATH = path + "app.js"
+
 
 while True:
     time.sleep(0.05)
     if keyboard.is_pressed('alt + u'):
-        command1 = "mkdir " + counter
-        command2 = "cp index.html " + counter + "/index.html"
-        command3 = "cp style.css " + counter + "/style.css"
-        command4 = "cd " + counter
-        command5 = "code app.js "
-        open_terminal()
-        typethis("cd " + FOLDER_LOCATION)
-        typethis(command1)
-        typethis(command2)
-        typethis(command3)
-        typethis(command4)
-        typethis(command5)
+        os.mkdir(path)        
+        shutil.copyfile(INDEX_HTML_PATH, (path + "index.html"))
+        shutil.copyfile(STYLE_CSS_PATH, (path + "style.css" ))
+        subprocess.run(["code.cmd", JS_PATH], check=True)
         addone()
